4ba8bf166f1760e7ece6b1bda8d2a83bc4e549f5
hbtch <gulnaz.baembitova@mail.ru>
2024-04-05 22:48:54 +0300

    chanhe app js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,6 @@ import Header from './components/Header/header.jsx';
 import Card from './components/Card/card.jsx';
 import Footer from './components/Footer/footer.jsx';
 
-const App = () => {
-  const data = [
-    {
-      "id": "15540",
-      "english": "pear",
-      "transcription": "[peə]",
-      "russian": "груша",
-      "tags": "fruit",
-      "tags_json": "[\"fruit\"]"
-    },
-    {
-      "id": "15544",
-      "english": "table",
-      "transcription": "[teɪbl]",
-      "russian": "стол",
-      "tags": "furniture",
-      "tags_json": "[\"furniture\"]"
-    },
-    {
-      "id": "15546",
-      "english": "laptop",
-      "transcription": "[læptɒp]",
-      "russian": "ноутбук",
-      "tags": "general",
-      "tags_json": "[\"general\"]"
-    },
-    {
-      "id": "15547",
-      "english": "tea",
-      "transcription": "[tiː]",
-      "russian": "чай",
-      "tags": "food",
-      "tags_json": "[\"food\"]"
-    },
-    {
-      "id": "15549",
-      "english": "example",
-      "transcription": "[example]",
-      "russian": "пример",
-      "tags": "general",
-      "tags_json": "[\"general\"]"
-    }
-  ];
-}
 function App() {
   return (
     <div className="App">
--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-function Card() {
+
+function card() {
     return (
         <header>
             <h1></h1>
@@ -8,4 +9,4 @@ function Card() {
     );
 }
 
-export default Card;
+export default card;
